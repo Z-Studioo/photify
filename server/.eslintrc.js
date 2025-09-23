52cbe47fb99c6542b8f07a38763e0e40ac5fddc7
Sajjan Karna <sajjan.kk@technimus.com>
2025-09-23 15:03:55 +0545

    feat(setup): setup gh actions, eslint in server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -6,36 +6,20 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    // Prettier integration
-    'prettier/prettier': 'error',
-
     // TypeScript specific rules
     '@typescript-eslint/no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unsafe-assignment': 'error',
-    '@typescript-eslint/no-unsafe-member-access': 'error',
-    '@typescript-eslint/no-unsafe-call': 'error',
-    '@typescript-eslint/no-unsafe-return': 'error',
-    '@typescript-eslint/prefer-readonly': 'error',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    '@typescript-eslint/prefer-optional-chain': 'error',
 
     // General rules
     'no-console': 'warn',
@@ -46,24 +30,14 @@ module.exports = {
     'prefer-template': 'error',
     'prefer-arrow-callback': 'error',
     'arrow-body-style': ['error', 'as-needed'],
-
-    // Import rules
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
+    'no-undef': 'off', // TypeScript handles this
+    'no-unused-vars': 'off', // Use TypeScript version instead
   },
   ignorePatterns: ['dist/', 'node_modules/', '*.js'],
   overrides: [
     {
       files: ['*.test.ts', '*.spec.ts'],
       rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
         'no-console': 'off',
       },
     },
