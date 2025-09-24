415a05f450e171b4744766d61fedec9408734b90
Sajjan Karna <sajjan.kk@technimus.com>
2025-09-24 15:49:56 +0545

    fix: resolve tailwind classes issue

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from './components/ui/button';
+
 export default function ThemeTest() {
   return (
     <>
-    <h1 className="text-center">Photify</h1>
+      <h1 className='text-center'>Photify</h1>
+
+      <Button>Button</Button>
     </>
   );
 }
