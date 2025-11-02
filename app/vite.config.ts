a2626290fd0958fc3901d5d822f415921ba35801
sandesh-dh <sandesh.dh@technimus.com>
2025-11-02 15:54:42 +0545

    fix(ui): decrease the width of sidebar(feature panel)

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,35 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),],
+//   resolve:{
+//     alias:{
+//       "@":path.resolve(__dirname,"./src"),
+//     }
+//   }
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src"),
-    }
-  }
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,       // allows access from network
+    port: 5173,       // you can change this if you want
+    strictPort: false, // if port is busy, Vite will try next available
+  },
 });
